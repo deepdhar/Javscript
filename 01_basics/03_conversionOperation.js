d06c6c5a5f7a8ec4ba439385d7c5daf42b99c4ec
Deep Dhar <dhar2017.slg@gmail.com>
2023-07-28 06:55:50 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,19 @@
+let score = true
+
+// console.log(typeof(score));
+// console.log(typeof score);
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => Nan (Not a Number), but typeof -> number
+// true => 1; false => 0
+
+
+let isLoggedIn = ""
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn);
